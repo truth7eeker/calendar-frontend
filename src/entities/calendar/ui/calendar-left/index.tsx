99ab9956bfa8050d85394bdf0b8fdbd0b487aa30
Elvira <el__1997@mail.ru>
2023-06-02 15:27:18 +0300

    fix rendered fewer hooks than expected

--- src/entities/calendar/ui/calendar-left/index.tsx
+++ src/entities/calendar/ui/calendar-left/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { IoTimeSharp, IoCalendarClear, IoEarth } from 'react-icons/io5';
 import moment from 'moment';
-import { useLocation } from 'react-router';
 import { getLocalStorage } from '../../../../shared';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../../app/store/store';
@@ -21,10 +20,10 @@ export function CalendarLeft() {
 
    const duration = time && moment(time, 'h:mm').add(15, 'minutes').format('h:mm');
   
-   const path = useLocation().pathname
+   const path = window.location.pathname;
 
    return (
-      <div className={styles.intro}>
+      <div className={path === '/' ? styles.intro : styles.intro__details}>
          <h3>Conversational lesson</h3>
          <p className={styles.intro__icons}>
             <IoTimeSharp />
