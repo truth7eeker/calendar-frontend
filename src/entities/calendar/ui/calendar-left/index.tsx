49bc2feccd3bf98ea6cf99e5fd501ccfe6f9f14f
Elvira <el__1997@mail.ru>
2023-06-02 15:35:45 +0300

    fix styles

--- src/entities/calendar/ui/calendar-left/index.tsx
+++ src/entities/calendar/ui/calendar-left/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { IoTimeSharp, IoCalendarClear, IoEarth } from 'react-icons/io5';
 import moment from 'moment';
+import { useLocation } from 'react-router';
 import { getLocalStorage } from '../../../../shared';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../../app/store/store';
@@ -20,10 +21,10 @@ export function CalendarLeft() {
 
    const duration = time && moment(time, 'h:mm').add(15, 'minutes').format('h:mm');
   
-   const path = window.location.pathname;
+   const path = useLocation().pathname
 
    return (
-      <div className={path === '/' ? styles.intro : styles.intro__details}>
+      <div className={styles.intro}>
          <h3>Conversational lesson</h3>
          <p className={styles.intro__icons}>
             <IoTimeSharp />
